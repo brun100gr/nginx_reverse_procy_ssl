d5df36f8270a2b2c5b0ccfe7720f614fc5fed474
Bruno Marelli <bruno.marelli@gmail.com>
2023-09-13 14:22:45 +0000

    Rework of esp32 sketch4
    - support to TLS/MQTTS
    - user/password authentication with MQTT broker
    - multiple topics subscribe
    - multiple topics publish

--- esp32/sketch4.ino
+++ esp32/sketch4.ino
@@ -1,49 +1,14 @@
-// Sketch che invia un topic MQTT utilizzando TLS
-/*
-  Secure connection example for ESP32 <----> Mosquitto broker (used for MQTT) communcation
-  with possible client authentication
-  Prerequisite:
-  PubSubClient library for Arduino - https://github.com/knolleary/pubsubclient/
-  OpenSSL - https://www.openssl.org/
-  Mosquitto broker - https://mosquitto.org/
-  
-  1. step - Generate the certificates
-  For generating self-signed certificates please run the following commands:
-      
-  openssl req -new -x509 -days 365 -extensions v3_ca -keyout ca.key -out ca.crt -subj '/CN=TrustedCA.net'  #If you generate self-signed certificates the CN can be anything
-  openssl genrsa -out mosquitto.key 2048
-  openssl req -out mosquitto.csr -key mosquitto.key -new -subj '/CN=Mosquitto_borker_adress'    #Its necessary to set the CN to the adress of which the client calls your Mosquitto server (eg. yourserver.com)!!!
-  openssl x509 -req -in mosquitto.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out mosquitto.crt -days 365 
-  #This is only needed if the mosquitto broker requires a client autentithication (require_certificate is set to true in mosquitto config)
-  openssl genrsa -out esp.key 2048
-  openssl req -out esp.csr -key esp.key -new -subj '/CN=localhost'
-  openssl x509 -req -in esp.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out esp.crt -days 365
-  2. Open ca.crt, esp.crt and esp.key with text viewer and copy the values to this WiFiClientSecureClientAuthentication.ino source file into 
-  corresponding const char CA_cert[], const char ESP_CA_cert[] and const char ESP_RSA_key[] with escape characters.
-  (1-2.) Alternatively you can use the certificates/certificate_generator.sh script 
-  for generating and formatting the certificates. Befor you run it, please modify the CN value for your adress, or modify any other settings based on yout requierments.
-  3. step - Install and setup your Mosquitto broker
-  Follow the instructions from https://mosquitto.org/ and check the manual for the configuration.
-  For the Mosquito broker you need ca.crt, mosquitto.key and mosquitto.crt files generated in previous step.
-  Recommended to put they in /etc/mosquitto/ca_certificates/ and /etc/mosquitto/certs/
-  You need to config Mosquitto broker to use these files (usually /etc/mosquitto/conf.d/default.conf):
-  
-  listener 8883
-  cafile path/to/ca.crt
-  keyfile path/to/mosquitto.key
-  certfile path/to/mosquitto.crt
-  require_certificate true or false #If you need client authentication set it to true
-  log_type all  #for logging in /var/log/mosquitto/
-  4.Restart the Mosquitto service or start the broker:
-  sudo service mosquitto restart
-  or
-  mosquitto -c /etc/mosquitto/conf.d/default.conf
-  
-  2021 - Norbert Gal - Apache 2.0 License.
-*/
-
-#include <PubSubClient.h>
 #include "WiFiClientSecure.h"
+#include <PubSubClient.h>
+
+// Replace the next variables with your SSID/Password combination
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+
+const char* mqtt_server = "brunomarelli.duckdns.org"; // Add your MQTT Broker address:
+const int mqtt_port     = 8883;     //Port to your Mosquitto broker server. Dont forget to forward it in your router for remote access
+const char* mqtt_user   = "bruno";  //Depends on Mosquitto configuration, if it is not set, you do not need it
+const char* mqtt_pass   = "";       //Depends on Mosquitto configuration, if it is not set, you do not need it
 
 const char* CA_cert = \
   "-----BEGIN CERTIFICATE-----\n" \
@@ -86,95 +51,115 @@ const char* ESP_RSA_key= \
   "-----BEGIN PRIVATE KEY-----\n" \
   "-----END PRIVATE KEY-----\n";
 
-const char* ssid        = "Vodafone-A76077447";        // your network SSID (name of wifi network)
-const char* password    = "";   // your network password
+WiFiClientSecure espClient;
+PubSubClient mqtt_client(espClient);
 
-const char* mqtt_server = "brunomarelli.duckdns.org";  //Adress for your Mosquitto broker server, it must be the same adress that you set in Mosquitto.csr CN field
-int port                = 8883;             //Port to your Mosquitto broker server. Dont forget to forward it in your router for remote access
-const char* mqtt_user   = "";           //Depends on Mosquitto configuration, if it is not set, you do not need it
-const char* mqtt_pass   = "";  //Depends on Mosquitto configuration, if it is not set, you do not need it
-
-WiFiClientSecure client;
-PubSubClient mqtt_client(client); 
+long lastMsg = 0;
 
 void setup() {
   Serial.begin(115200);
-  delay(100); 
 
-  Serial.print("Attempting to connect to SSID: ");
+  setup_wifi();
+  //Set up the certificates and keys
+  espClient.setCACert(CA_cert);          //Root CA certificate
+  //espClient.setCertificate(ESP_CA_cert); //for client verification if the require_certificate is set to true in the mosquitto broker config
+  //espClient.setPrivateKey(ESP_RSA_key);  //for client verification if the require_certificate is set to true in the mosquitto broker config
+
+  mqtt_client.setServer(mqtt_server, mqtt_port);
+  mqtt_client.setCallback(callback);
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
   Serial.println(ssid);
+
   WiFi.begin(ssid, password);
 
-  // attempt to connect to Wifi network:
   while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
     Serial.print(".");
-    // wait 1 second for re-trying
-    delay(1000);
   }
 
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-
-  //Set up the certificates and keys
-  client.setCACert(CA_cert);          //Root CA certificate
-  client.setCertificate(ESP_CA_cert); //for client verification if the require_certificate is set to true in the mosquitto broker config
-  client.setPrivateKey(ESP_RSA_key);  //for client verification if the require_certificate is set to true in the mosquitto broker config
-
-  mqtt_client.setServer(mqtt_server, port);
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {  
-  Serial.println("\nStarting connection to server...");
-  //if you use password for Mosquitto broker
-  //if (mqtt_client.connect("ESP32", mqtt_user , mqtt_pass)) {
-  //if you dont use password for Mosquitto broker
-  if (mqtt_client.connect("ESP32")) {                       
-    Serial.print("Connected, mqtt_client state: ");
-    Serial.println(mqtt_client.state());
-    //Publsih a demo message to topic LivingRoom/TEMPERATURE with a value of 25
-    mqtt_client.publish("LivingRoom/TEMPERATURE", "25");
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
   }
-  else {
-    Serial.println("Connected failed!  mqtt_client state:");
-    Serial.print(mqtt_client.state());
-    Serial.println("WiFiClientSecure client state:");
-    char lastError[100];
-    client.lastError(lastError,100);  //Get the last error for WiFiClientSecure
-    Serial.print(lastError);
+  Serial.println();
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  if (String(topic) == "esp32/output") {
+    Serial.print("Changing output to ");
+    if(messageTemp == "on"){
+      Serial.println("on");
+    }
+    else if(messageTemp == "off"){
+      Serial.println("off");
+    }
   }
-  delay(10000);
 }
 
+void reconnect() {
+  // Loop until we're reconnected
+  while (!mqtt_client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (mqtt_client.connect("ESP32", mqtt_user , mqtt_pass)) {
+      Serial.println("connected");
+      // Subscribe
+      mqtt_client.subscribe("esp32/output1");
+      mqtt_client.subscribe("esp32/output2");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqtt_client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
+float topicA = 0.0;
+float topicB = 0.5;
+void loop() {
+  if (!mqtt_client.connected()) {
+    reconnect();
+  }
+  mqtt_client.loop();
 
-
-
-#################################################################################################################################################################################
-#Certificate generator for TLS encryption																	#
-#################################################################################################################################################################################
-openssl req -new -x509 -days 365 -extensions v3_ca -keyout ca.key -out ca.crt -passout pass:1234 -subj '/CN=TrustedCA.net'
-#If you generating self-signed certificates the CN can be anything
-
-openssl genrsa -out mosquitto.key 2048
-openssl req -out mosquitto.csr -key mosquitto.key -new -subj '/CN=locsol.dynamic-dns.net'
-openssl x509 -req -in mosquitto.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out mosquitto.crt -days 365 -passin file:PEMpassphrase.pass
-#Mostly the client verifies the adress of the mosquitto server, so its necessary to set the CN to the correct adress (eg. yourserver.com)!!!
-
-
-#################################################################################################################################################################################
-#These certificates are only needed if the mosquitto broker requires a certificate for client autentithication (require_certificate is set to true in mosquitto config).      	#
-#################################################################################################################################################################################
-openssl genrsa -out esp.key 2048
-openssl req -out esp.csr -key esp.key -new -subj '/CN=localhost'
-openssl x509 -req -in esp.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out esp.crt -days 365 -passin pass:1234
-#If the server (mosquitto) identifies the clients based on CN key, its necessary to set it to the correct value, else it can be blank.
-
-#################################################################################################################################################################################
-#For ESP32. Formatting for source code. The output is the esp_certificates.c                                                                                                    #
-#################################################################################################################################################################################
-echo -en "#include \"esp_certificates.h\"\n\nconst char CA_cert[] = \\\\\n" > esp_certificates.c;
-sed -z '0,/-/s//"-/;s/\n/\\n" \\\n"/g;s/\\n" \\\n"$/";\n\n/g' ca.crt >> esp_certificates.c     #replace first occurance of - with "-  ;  all newlnies with \n " \ \n", except last newline where just add ;"
-echo "const char ESP_CA_cert[] = \\" >> esp_certificates.c;
-sed -z '0,/-/s//"-/;s/\n/\\n" \\\n"/g;s/\\n" \\\n"$/";\n\n/g' esp.crt >> esp_certificates.c     #replace first occurance of - with "-  ;  all newlnies with \n " \ \n", except last newline where just add ;"
-echo "const char ESP_RSA_key[] = \\" >> esp_certificates.c;
-sed -z '0,/-/s//"-/;s/\n/\\n" \\\n"/g;s/\\n" \\\n"$/";/g' esp.key >> esp_certificates.c     #replace first occurance of - with "-  ;  all newlnies with \n " \ \n", except last newline where just add ;"
+  long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+    
+    // Convert the value to a char array
+    char topicAString[8];
+    dtostrf(topicA, 1, 2, topicAString);
+    Serial.print("TopicA: ");
+    Serial.println(topicAString);
+    mqtt_client.publish("esp32/topicA", topicAString);
+    topicA++;
+       
+    // Convert the value to a char array
+    char topicBString[8];
+    dtostrf(topicB, 1, 2, topicBString);
+    Serial.print("TopicB: ");
+    Serial.println(topicBString);
+    mqtt_client.publish("esp32/topicB", topicBString);
+    topicB++;
+  }
+}
